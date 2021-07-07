f08b8f75c8ed44d3a1f17897dd3497ee63a6fdcd
RaulSE98 <56968210+RaulSE98@users.noreply.github.com>
2021-07-07 12:08:39 +0200

    Create Ejercicio2.java

---
+++ Ejercicio2.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int N = 5;
+		double A = 4.56;
+		char C = 'a';
+		double r;
+		int x;
+		
+		System.out.println("valor inicial de N = " + N);
+		System.out.println("valor inicial de A = " + A);
+		System.out.println("valor inicial de C = " + C);
+		
+		//Suma
+		System.out.println("resultado N + A = " + (r = N + A));
+		//Diferencia
+		System.out.println("resultado A - N = " + (r = A - N));
+		//Numero char
+		System.out.println("valor numerico de C = " + (x = C));
+
+	}
+
+}
